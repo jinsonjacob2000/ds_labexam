25a7a34c25026d0d761fa45eeac137cf81103ffa
jinsonjacob2000 <94525804+jinsonjacob2000@users.noreply.github.com>
2022-03-10 12:57:26 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,28 @@
+
+# include <stdio.h>
+void main()
+
+
+{
+int i=0,a[10],b[20],c[30],n1,n2;
+ printf("enter the limit of the first set");
+ scanf("%d",&n1);
+
+ printf("enter the elements in set a");
+ for(int i=0;i<n1;i++)
+
+ {
+ scanf("%d",&a[i]);
+
+ }
+
+ printf("enter the limit of the second set");
+ scanf("%d",&n2);
+
+ printf("enter the elements in second set");
+ for(int i=0;i<n2;i++)
+ {
+  scanf("%d",&b[i]);
+ }
+ printf("the elements in first set is,%d",&a[i]);
+}
